c0ec03b8265e5bf4df827aac19ad1f7029e2ddd0
Aman Sharma <mannu.poski10@gmail.com>
2020-01-02 18:00:54 +0530

    Make CMS intergration optional

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,24 +1,16 @@
 import React from "react";
-import { Card, Icon, Image, Dimmer, Segment, Label, Button } from "semantic-ui-react";
+import { Card, Icon, Image, Dimmer, Segment } from "semantic-ui-react";
 
 import axios from "axios";
-import { toast } from 'react-semantic-toasts';
 
 import DefaultDP from "../../../../../formula_one/src/components/default-dp";
 import { getCookie } from "formula_one";
 
-import { headers, jsonHeaders } from "../../constants/formPostRequestHeaders";
 import { ProfileForm } from "./profileForm";
-import { PublishManager } from "./publishManager";
-
-import { BrowserView, MobileView } from "react-device-detect";
+import { urlCmsIntegration } from "../../urls"
 
 import style from "../../styles.css";
 
-const a_style = {
-  'color' : 'green',
-  'textDecoration' : 'underline'
-}
 export class Profile extends React.Component {
   constructor(props) {
     super(props);
@@ -37,10 +29,8 @@ export class Profile extends React.Component {
       active: false,
       createNew: true,
       image: null,
-      button_load:{
-        preview: false,
-        publish: false
-      }
+      cmsIntegration: false,
+      CmsIntegrationComponent: <></>
     };
   }
   componentDidMount() {
@@ -72,7 +62,24 @@ export class Profile extends React.Component {
       .catch(function (error) {
         // console.error(error);
       });
-    Promise.all([person_promise, faculty_promise]).then(() => this.setState({ loading: false }));
+    const cmsIntegrationPromise = axios
+      .get(urlCmsIntegration())
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({
+            cmsIntegration: true,
+            CmsIntegrationComponent:
+              React.lazy(() => import('./cmsIntegration'))
+          }, () => {
+
+          })
+        }
+      })
+      .catch(error => {
+        console.error(error.response.data);
+      });
+    Promise.all([person_promise, faculty_promise, cmsIntegrationPromise])
+    .then(() => this.setState({ loading: false }));
   };
 
   handleShow = e => {
@@ -90,107 +97,21 @@ export class Profile extends React.Component {
   };
   handleHide = () => {
     this.setState({ active: false });
-  };
-
-  onPreview = () => {
-    this.setState({
-      button_load:{
-        ... this.state.button_load,
-        preview: true
-      }
-    });
-    axios({
-      method: "POST",
-      url: "api/faculty_profile/preview",
-      data: JSON.stringify({'action': 'preview'}),
-      headers: jsonHeaders
-    })
-      .then(response => {
-        const data = response.data;
-        const url = "http://cms.channeli.in" + data['url'];
-        const description = <p>Click on this  <a href={url} style={a_style} target="_blank">link</a> to preview your profile page </p>
-        toast({
-          title: 'Preview Profile Page',
-          icon: 'print',
-          description: description,
-          time: 5000
-        });
-        return 'success';
-      })
-      .catch(error => {
-        // console.error(error);
-        toast({
-          type: 'error',
-          title: 'Preview Error',
-          icon: 'print',
-          description: <p>Some error has occurred. Try previewing your page after some time!</p>,
-          time: 2000
-        });
-        return 'error';
-      })
-      .finally(code => {
-        this.setState({
-          button_load:{
-            ... this.state.button_load,
-            preview: false
-          }
-        });       
-      });
-  }
-
-  onPublish = () => {
-    this.setState({
-      button_load:{
-        ... this.state.button_load,
-        publish: true
-      }
-    });
-    axios({
-      method: "POST",
-      url: "api/faculty_profile/preview",
-      data: JSON.stringify({'action':'publish'}),
-      headers: jsonHeaders
-    })
-      .then(response => {
-        let data = response.data;
-        const url = "http://iitr.ac.in" + data['url'];
-        const description = <p>Your page will be published at <a href={url} style={a_style} target="_blank">{ url }</a> <br/>Wait some time for content to update.</p>
-        toast({
-          title: 'Preview Profile Page',
-          icon: 'print',
-          description: description,
-          time: 5000
-        });
-        return 'success';
-      })
-      .catch(error => {
-        // console.error(error);
-        toast({
-          type: 'error',
-          title: 'Publish Error',
-          icon: 'upload',
-          description: <p>Some error has occurred. Try publishing your page after some time!</p>,
-          time: 2000
-        });
-        return 'error';
-      })
-      .finally(code =>{
-        this.setState({
-          button_load:{
-            ... this.state.button_load,
-            publish: false
-          }
-        });
-      });
-  }
+  };  
 
   render() {
     const desc = this.state.data.description;
     let { theme, changeTheme } = this.props;
     theme = "blue";
-    const {handleHide, handleUpdate, onPreview, onPublish} = this;
-    const { data, person_data, createNew, loading, button_load} = this.state;
-    const ownHandle = data.handle;
+    const {handleHide, handleUpdate} = this;
+    const {
+      data,
+      person_data,
+      createNew,
+      loading,
+      cmsIntegration,
+      CmsIntegrationComponent
+    } = this.state;
     const style = {
       boxShadow: "0 0 0 1px #d4d4d5,0 2px 0 0 #d4d4d5,0 1px 3px 0 #d4d4d5"
     };
@@ -234,7 +155,11 @@ export class Profile extends React.Component {
             </Dimmer>
           </Card>
 
-          <PublishManager preview={onPreview} publish={onPublish} theme={theme} loading={button_load} />
+          {cmsIntegration && (
+            <CmsIntegrationComponent
+              theme={theme}
+            />
+          )}
         </div>
       );
   }
---
+++ src/urls.js
@@ -0,0 +1,15 @@
+export function urlMainApp() {
+  return '/api/faculty_profile/';
+}
+
+export function urlCmsIntegration() {
+  return `${urlMainApp()}cms/`;
+}
+
+export function urlCms() {
+  return 'https://cms.channeli.in';
+}
+
+export function urlIITRMain() {
+  return 'https://iitr.ac.in'
+}
