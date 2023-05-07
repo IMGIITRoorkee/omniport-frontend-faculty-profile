4ef218994256e5a7502330a008c74da277fcd259
samratmiddha <s_middha@me.iitr.ac.in>
2023-05-08 03:41:10 +0530

    Add shorturl rejection description and Add declined request icon

--- src/components/displayComponents/shorturl.js
+++ src/components/displayComponents/shorturl.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { List, Segment } from 'semantic-ui-react'
+import { Icon, List, Segment } from 'semantic-ui-react'
 import { EditIcon } from '../editIcon'
 import style from '../../styles.css'
 
 const ShortURL = props => {
+  if(props.item.approvalStatus==='pen'){
+    props.item.rejectionReason="shortutl yet to be approved"
+  }
 
   const rows = []
   for (let index in props.item.allUrls) {
@@ -12,16 +15,23 @@ const ShortURL = props => {
         <div styleName='style.flex-box'>
           <List styleName='style.list'>
             <List.Item>
-              {props.item.approvalStatus === 'app' ? <List.Icon name='angle double right' /> : <List.Icon name='question' /> }
+              {props.item.approvalStatus === 'app' ? <List.Icon name='angle double right' /> : props.item.approvalStatus === 'dec'?<List.Icon name='exclamation' />:<List.Icon name='question' /> }
               <List.Content>{props.item.allUrls[index]}</List.Content>
             </List.Item>
           </List>
-
+         <div styleName='style.flex-box'>
+          {props.item.approvalStatus!=='app'?(
+          <div data-tooltip={props.item.rejectionReason} data-position="left center">
+          <Icon name='info circle' ></Icon>
+          </div>
+          ):(<></>)
+          }
           <EditIcon
             onClick={() =>
               props.manageData(props.item.id, props.data, props.componentName)
             }
           />
+          </div>
         </div>
       </Segment>
     )
