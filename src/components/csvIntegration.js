10cf473071ca410fa5c8efb56a533ccc4f0cd024
lakshita10341 <lakshitajainchopra89@gmail.com>
2026-08-31 12:19:59 +0530

    fix: set name=uploadType on New radio so it can be selected in file upload modal

--- src/components/csvIntegration.js
+++ src/components/csvIntegration.js
@@ -226,7 +226,7 @@ export class WriteAppendMultipleObjects extends Component {
               <Form.Field>
                 <Radio
                   label="New"
-                  name=""
+                  name="uploadtype"
                   value="new"
                   checked={data.uploadType === "new"}
                   onChange={this.handleChange}
